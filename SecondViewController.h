1b5696e754c48369348dea945d5932c8778323b1
Michal <michalpragacz2@gmail.com>
2023-11-12 11:45:20 +0100

    Zadanie 4

---
+++ SecondViewController.h
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.h
+//  Zadanie 1 iOS
+//
+//  Created by student on 30/10/2023.
+//  Copyright © 2023 PolitechnikaBiałostocka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
